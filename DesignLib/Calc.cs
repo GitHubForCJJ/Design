8bffb622f21dd0467a6ca50cf53ff01479bc72e8
chenjianjun <chenjianjun@567na.com>
2019-12-02 18:28:03 +0800

    简单工厂

---
+++ DesignLib/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignLib
+{
+    public abstract class Calc
+    {
+        public string NumberA { get; set; }
+        public string NumberB { get; set; }
+
+        public abstract Double GetResult();
+    }
+}
